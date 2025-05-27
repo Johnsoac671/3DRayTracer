dab99b4c394e4f24fb90d0efdeca99864fd08d8b
Aidan Johnson <johnsoac671@gmail.com>
2025-05-27 08:57:20 -0700

    beginning to refactor gross Camera class

---
+++ Rendering Engine/RenderSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendering_Engine
+{
+    public class RenderSettings
+    {
+
+        public int SamplesPerPixel { get; init; } = 100;
+        public int MaxDepth { get; init; } = 50;
+
+        public double SampleScale => 1.0 / SamplesPerPixel;
+    }
+}
