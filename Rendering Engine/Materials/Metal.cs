dd4f4030eb2b024e22770944146fae591e6138e2
Aidan Johnson <johnsoac671@gmail.com>
2025-05-26 17:28:46 -0700

    added roughness option to Metallic Material

--- Rendering Engine/Materials/Metal.cs
+++ Rendering Engine/Materials/Metal.cs
@@ -10,15 +10,18 @@ namespace Rendering_Engine.Materials
     internal class Metal : Material
     {
         public Color3 Albedo { get; }
+        public double Roughness {  get; }
 
-        public Metal(Color3 albedo)
+        public Metal(Color3 albedo, double roughness = 0.0)
         {
             this.Albedo = albedo;
+            this.Roughness = roughness < 1 ? roughness : 1;
         }
 
         public override Ray Scatter(Ray r, HitRecord record, ref Color3 attenunation)
         {
             Vector3 reflected = Vector3.Reflect(r.Direction, record.Normal);
+            reflected = Vector3.UnitVector(reflected) + (this.Roughness * Vector3.RandomUnitVector());
             Ray scatteredRay = new Ray(record.Location, reflected);
             attenunation = this.Albedo;
 
