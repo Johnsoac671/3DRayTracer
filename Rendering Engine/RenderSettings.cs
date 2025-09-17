f00d87e95b935dd4c1c96c7b500bcb2ef13d3c1e
Aidan Johnson <johnsoac671@gmail.com>
2025-09-17 16:17:14 -0700

    updated render settings for nicer test render

--- Rendering Engine/RenderSettings.cs
+++ Rendering Engine/RenderSettings.cs
@@ -9,13 +9,13 @@ namespace Rendering_Engine
     public class RenderSettings
     {
 
-        public int SamplesPerPixel { get; init; } = 100;
+        public int SamplesPerPixel { get; init; } = 50;
         public int MaxDepth { get; init; } = 50;
 
         public double SampleScale => 1.0 / SamplesPerPixel;
 
         public double AspectRatio { get; init; } = 16.0 / 9.0;
-        public int ImageWidth { get; init; } = 128;
+        public int ImageWidth { get; init; } = 600;
         public int ImageHeight => Math.Max((int)(ImageWidth / AspectRatio), 1);
     }
 }
